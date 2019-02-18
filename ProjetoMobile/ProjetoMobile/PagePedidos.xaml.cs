c317895b0fb73716762bc1a279ccdd2295d70dc1
mateusjrocha.ruim <mateusjrocha.ruim@gmail.com>
2019-02-18 15:34:44 -0300

    Add project files.

---
+++ ProjetoMobile/ProjetoMobile/PagePedidos.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProjetoMobile
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PagePedidos : ContentPage
+	{
+		public PagePedidos ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
